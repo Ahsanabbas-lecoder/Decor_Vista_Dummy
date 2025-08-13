408e399b00efa03f0ff313119aec6823125adbba
Ahsanabbas-lecoder <ahsanabbaskhican@gmail.com>
2025-08-13 11:44:34 +0500

    Update form.py

--- proweb/proapp/form.py
+++ proweb/proapp/form.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     name = forms.CharField(max_length=100, required=True)
     gmail = forms.EmailField(label="Email", required=True)
-    role = forms.ChoiceField(choices=[('Designer','Designer'), ('Inferior','Inferior')], required=True)
+    role = forms.ChoiceField(choices=[('Designer','Designer'), ('User','User')], required=True)
 
     class Meta:
         model = User
